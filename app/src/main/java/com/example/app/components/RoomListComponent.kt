d37055bac45092c3314ca4e289da77ca242b28ae
Khalilov Shokhboz <shahboz.halilov@yandex.com>
2023-03-17 22:30:10 +0300

    SH-04 room list component

---
+++ app/src/main/java/com/example/app/components/RoomListComponent.kt
@@ -0,0 +1,28 @@
+package com.example.ai_home.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.ai_home.RoomData
+
+@Composable
+fun RoomListComponent(onRoomChange: (id: Int) -> Unit, rooms: List<RoomData>) {
+    var current by remember { mutableStateOf(0) }
+
+    fun roomChange(id: Int) {
+        onRoomChange(id)
+        current = id
+    }
+
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(8.dp), modifier = Modifier.padding(4.dp)
+    ) {
+        items(rooms) { room ->
+            RoomComponent(room, onClicked = { id -> roomChange(id) }, selected = current == room.id)
+        }
+    }
+}
