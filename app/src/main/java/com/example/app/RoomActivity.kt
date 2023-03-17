bfb6ca070a146b679a918e4227d891fa7cebd31c
Khalilov Shokhboz <shahboz.halilov@yandex.com>
2023-03-17 22:34:00 +0300

    SH-08 room activity

---
+++ app/src/main/java/com/example/app/RoomActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ai_home
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class RoomActivity : ComponentActivity()  {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {}
+    }
+}
