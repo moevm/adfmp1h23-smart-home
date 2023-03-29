1ba90ad9273f5647e4ebe5ec438067f886fbd5b7
Maxim Efimov <maksimyss2000@mail.ru>
2023-03-30 02:36:56 +0300

    jump

--- app/src/main/java/com/example/smarthome/MainActivity.kt
+++ app/src/main/java/com/example/smarthome/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.smarthome
 
+import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import com.example.smarthome.R.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val vText = findViewById<TextView>(id.push_me)
+        vText.setOnClickListener{
+            Log.e("tag", "test")
+            val i= Intent(this, SecondActivity::class.java)
+            startActivity(i)
+        }
     }
+
 }
---
+++ app/src/main/java/com/example/smarthome/SecondActivity.kt
@@ -0,0 +1,16 @@
+package com.example.smarthome
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.*
+
+
+class SecondActivity: Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.second_activity)
+    }
+}
