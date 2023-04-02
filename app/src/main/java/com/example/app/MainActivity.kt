3aeb6ec0c2510abaff04475da4fea8083b17e2a2
dyukoffv4 <net998art@gmail.com>
2023-04-02 12:50:57 +0300

    navigation + project theme

--- app/src/main/java/com/example/app/MainActivity.kt
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -1,101 +1,36 @@
-package com.example.ai_home
+package com.example.app
 
 import android.annotation.SuppressLint
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
-import androidx.compose.animation.*
-import androidx.compose.foundation.*
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.*
-import androidx.compose.foundation.shape.*
-import androidx.compose.material.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.*
-import com.example.ai_home.DeviceActivity
-import com.example.ai_home.components.*
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.app.navigation.Navigation
+import com.example.app.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
-    private val viewModel by viewModels<CardViewModel>()
 
     @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter", "StateFlowValueCalledInComposition")
-    @OptIn(ExperimentalMaterial3Api::class)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val rooms = mutableListOf(
-            RoomData(0, "all")
-        )
-
-        fun addNewRoom(id: Int, title: String) {
-            rooms.add(RoomData(id, title))
-        }
-
         setContent {
-            var selectedRoom by remember { mutableStateOf(rooms[0]) }
-            val openDialog = remember { mutableStateOf(false) }
-            val addRoom = remember { mutableStateOf(false) }
-            var roomCount by remember { mutableStateOf(1) }
-            val editMode = remember { mutableStateOf(false) }
-            val cards by viewModel.cards.collectAsState()
-
-            fun onAddDeviceClicked() {
-                val intent = Intent(this, DeviceActivity::class.java)
-                startActivity(intent)
-            }
-
-            Scaffold(bottomBar = {
-                BottomAppBarComponent(
-                    onAddClicked = { openDialog.value = true },
-                    onEditClicked = {
-                        editMode.value = !editMode.value
-                    } // pass a lambda function to toggle editMode
-                )
-            }) {
-                Column {
-                    RoomListComponent(
-                        onRoomChange = { id ->
-                            selectedRoom = if (id == 0) {
-                                rooms[0] // "all" room
-                            } else {
-                                rooms.find { it.id == id }
-                                    ?: rooms[0] // fallback to "all" room if not found
-                            }
-                        },
-                        rooms
-                    )
-
-                    val filterd = if(selectedRoom.id == 0) cards else cards.filter { card -> card.roomId == selectedRoom.id }
-                    CardListComponent(
-                        filterd,
-                        editMode,
-                        onRemoveClick = { id -> viewModel.removeCard(id) })
-                }
-
-                if (openDialog.value) {
-                    AddComponent(
-                        onAddDeviceClick = { onAddDeviceClicked() },
-                        onAddRoomClick = {
-                            addRoom.value = true
-                            openDialog.value = false
-                        }
-                    )
-                }
-
-                if (addRoom.value) {
-                    AddRoomComponent(
-                        onCancelClick = { addRoom.value = false },
-                        onSaveClick = { title ->
-                            addNewRoom(roomCount++, title)
-                            addRoom.value = false
-                        }
+            AppTheme{
+                Surface(
+                    modifier = Modifier
+                        .fillMaxSize()
+                ) {
+                    Navigation(
+                        modifier = Modifier.fillMaxSize()
                     )
                 }
             }
         }
     }
 }
+
 
--- app/src/main/java/com/example/app/ui/theme/Color.kt
+++ app/src/main/java/com/example/app/ui/theme/Color.kt
@@ -2,10 +2,14 @@ package com.example.app.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val ProjectBlack = Color(0xFF1C1B1F)
+val White = Color(0xFFFFFFFF)
+val White35 = Color(0x59FFFFFF)
+val White50 = Color(0x59FFFFFF)
+val PurpleMedium = Color(0xFF4A4458)
+val PurpleLight = Color(0xFFE8DEF8)
+val PurpleDark = Color(0xFF1C1B1F)
+val BlueLight = Color(0xFF0FCAE3)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val PurpleAnother = Color(0xFFE8DEF8)
+val Green = Color(0xFF80DF8A)
